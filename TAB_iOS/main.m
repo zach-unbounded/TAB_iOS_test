3c7cf1f8efb85e385bba7b83075202b9af7c41a7
Zachary BURGESS <zac.unbounded@gmail.com>
2014-07-02 10:39:19 +0100

    inittial commit

---
+++ TAB_iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TAB_iOS
+//
+//  Created by Zachary BURGESS on 02/07/2014.
+//  Copyright (c) 2014 Zachary BURGESS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TABAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TABAppDelegate class]));
+    }
+}
